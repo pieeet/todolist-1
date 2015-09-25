7447813ad4c302e2970a04e7731bc070abc5c379
Piet de Vries <p.h.devries@gmail.com>
2015-09-25 16:24:12 +0200

    werken aan DB klasse

--- Takenlijst/app/src/main/java/com/rocdev/android/takenlijst/TakenlijstDB.java
+++ Takenlijst/app/src/main/java/com/rocdev/android/takenlijst/TakenlijstDB.java
@@ -1,11 +1,20 @@
 package com.rocdev.android.takenlijst;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by piet on 23-09-15.
  */
 public class TakenlijstDB {
 
-    //database constanten
+    //database constanten om de tabellen te creëren of droppen
     public static final String DB_NAAM  = "takenlijst.db";
     public static final int DB_VERSIE = 1;
 
@@ -57,6 +66,130 @@ public class TakenlijstDB {
             "DROP TABLE IF EXISTS " + LIJST_TABEL;
 
     public static final String DROP_TAAK_TABEL =
-            "DROP TABLE IF EXISTS " + LIJST_TABEL;
+            "DROP TABLE IF EXISTS " + TAAK_TABEL;
+
+    //database en databaseHelper object
+    private SQLiteDatabase db;
+    private DBHelper dbHelper;
+
+    public TakenlijstDB(Context context) {
+        dbHelper = new DBHelper(context, DB_NAAM, null, DB_VERSIE);
+    }
+
+    //hulpmethodes
+    private void openReadableDB() {
+        db = dbHelper.getReadableDatabase();
+    }
+
+    private void openWritableDB() {
+        db = dbHelper.getWritableDatabase();
+    }
+
+    private void closeDB() {
+        db.close();
+    }
+
+    public ArrayList<Taak> getTaken(String lijstNaam) {
+
+        String where = TAAK_LIJST_ID + "= ? AND " +
+                TAAK_VERBORGEN + "!='1'";
+        int listID = getLijst(lijstNaam).getId();
+        String[] whereArgs = { Integer.toString(listID)};
+        this.openReadableDB();
+        Cursor cursor = db.query(TAAK_TABEL, null, where, whereArgs, null, null, null);
+        ArrayList<Taak> taken = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            taken.add(getTaakVanCursor(cursor));
+        }
+        return taken;
+    }
+
+    public Taak getTaak(int id) {
+        String where = TAAK_ID + "= ?";
+        String[] whereArgs = { Integer.toString(id) };
+        this.openReadableDB();
+        Cursor cursor = db.query(TAAK_TABEL, null, where, whereArgs, null, null, null);
+        cursor.moveToFirst();
+        Taak taak = getTaakVanCursor(cursor);
+        if (cursor != null) {
+            this.closeDB();
+        }
+        return taak;
+    }
+
+    public Lijst getLijst(String name) {
+        String where = LIJST_NAAM + "= ?";
+        String[] whereArgs = { name };
+
+        openReadableDB();
+        Cursor cursor = db.query(LIJST_TABEL, null,
+                where, whereArgs, null, null, null);
+        Lijst lijst = null;
+        cursor.moveToFirst();
+        lijst = new Lijst(cursor.getInt(LIJST_ID_COL),
+                cursor.getString(LIJST_NAAM_COL));
+        if (cursor != null)
+            cursor.close();
+        this.closeDB();
+
+        return lijst;
+    }
+
+    private static Taak getTaakVanCursor(Cursor cursor) {
+        if (cursor == null || cursor.getCount() == 0){
+            return null;
+        }
+        else {
+            try {
+                Taak taak = new Taak();
+                taak.setTaakId(cursor.getInt(TAAK_ID_COL));
+                taak.setLijstId(cursor.getInt(TAAK_LIJST_ID_COL));
+                taak.setNaam(cursor.getString(TAAK_NAAM_COL));
+                taak.setNotitie(cursor.getString(TAAK_NOTITIE_COL));
+                taak.setAfgerond(cursor.getString(TAAK_AFGEROND_COL));
+                taak.setVerborgen(cursor.getString(TAAK_VERBORGEN_COL));
+                return taak;
+            }
+            catch(Exception e) {
+                return null;
+            }
+        }
+    }
+
+
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+
+
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_LIJST_TABLE);
+            db.execSQL(CREATE_TAAK_TABLE);
+            //paar default lijsten invoeren
+            db.execSQL("INSERT INTO lijst VALUES (1, Persoonlijk");
+            db.execSQL("INSERT INTO lijst VALUES (2, Zakelijk");
+            //paar default taken invoeren
+            db.execSQL("INSERT INT0 taak VALUES (1, 1, 'Rekeningen betalen'" +
+            "'huur\ntelefoon\ninternet', 0, 0)");
+            db.execSQL("INSERT INT0 taak VALUES (2, 1, 'Naar kapper'" +
+                    "'', 0, 0)");
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.d("Takenlijst", "upgraden db versie van " +
+            oldVersion + " naar " + newVersion);
+            db.execSQL(DROP_LIJST_TABEL);
+            db.execSQL(DROP_TAAK_TABEL);
+            onCreate(db);
+
+        }
+    }
 
 }
